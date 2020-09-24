9b8c223c7b2e705b1ad01d7ea8415e66f0c06926
Jóhann Garðarsson <gardarsson@outlook.com>
2020-09-24 20:27:16 +0000

    Added more models for nursery - created a script to switch between cameras

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,10 +7,21 @@ public class CameraController : MonoBehaviour
     public float speed;
 
     private Transform cameraTransform;
+    private Material fadeMaterial;
 
-    void Start()
+    void OnEnable()
     {
         cameraTransform = transform;
+        fadeMaterial = GetComponentInChildren<MeshRenderer>().material;
+        fadeMaterial.color = new Color(fadeMaterial.color.r, fadeMaterial.color.g, fadeMaterial.color.b, 1);
+
+        StartCoroutine(FadeMaterial(fadeMaterial));
+    }
+
+    void OnDisable()
+    {
+        fadeMaterial = GetComponentInChildren<MeshRenderer>().material;
+        fadeMaterial.color = new Color(fadeMaterial.color.r, fadeMaterial.color.g, fadeMaterial.color.b, 1);
     }
     
     void Update()
@@ -19,5 +30,14 @@ public class CameraController : MonoBehaviour
             cameraTransform.localPosition += cameraTransform.right * (-speed * Time.deltaTime);
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
             cameraTransform.localPosition += cameraTransform.right * (speed * Time.deltaTime);
+    }
+
+    private IEnumerator FadeMaterial(Material mat)
+    {
+        for(float i = 1; i >= 0; i -= 0.05f)
+        {
+            mat.color = new Color(mat.color.r, mat.color.g, mat.color.b, i);
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 }
---
+++ Assets/Scripts/CameraSwitcher.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    public GameObject livingRoomCamera, nurseryCamera;
+
+    void OnMouseDown()
+    {
+        livingRoomCamera.gameObject.SetActive(!livingRoomCamera.gameObject.active);
+        nurseryCamera.gameObject.SetActive(!nurseryCamera.gameObject.active);
+    }
+}
