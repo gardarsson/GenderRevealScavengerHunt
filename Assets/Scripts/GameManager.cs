4d9ecea041a4b43fdac3cd4ee853e26345844f36
Anna Lára <annasig95@gmail.com>
2020-09-24 22:47:23 +0000

    start of questions

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private Question currentQuestion;
+    
+    [SerializeField]
+    private Text question;
+
+    [SerializeField]
+    private Text answer1;
+
+    [SerializeField]
+    private Text answer2;
+
+    [SerializeField]
+    private Text answer3;
+
+    [SerializeField]
+    private Text answer4;
+
+    private int correctAnswer;
+
+    void SetQuestion()
+    {
+        question.text = currentQuestion.question;
+        answer1.text = currentQuestion.answers[0];
+        answer2.text = currentQuestion.answers[1];
+        answer3.text = currentQuestion.answers[2];
+        answer4.text = currentQuestion.answers[3];
+        correctAnswer = currentQuestion.correctAnswer;
+    }
+
+}
---
+++ Assets/Scripts/Question.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Question : MonoBehaviour
+{
+    public string question;
+    public string[] answers;
+    public int correctAnswer;
+  
+}
