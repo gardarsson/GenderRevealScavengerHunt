32920cf030a00d664eeb325e52f7b0150bb4eaa5
Jóhann Garðarsson <gardarsson@outlook.com>
2020-09-30 00:01:17 +0000

    Added UI prompt for transitioning between the two different rooms

--- Assets/Scripts/CameraSwitcher.cs
+++ Assets/Scripts/CameraSwitcher.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CameraSwitcher : MonoBehaviour
 {
     public GameObject livingRoomCamera, nurseryCamera;
+    public TextMeshProUGUI textToToggle;
 
-    void OnMouseDown()
+    public void ToggleCameras()
     {
         livingRoomCamera.gameObject.SetActive(!livingRoomCamera.gameObject.active);
         nurseryCamera.gameObject.SetActive(!nurseryCamera.gameObject.active);
+
+        if (!livingRoomCamera.activeInHierarchy)
+            textToToggle.text = "Do you want to enter the living room?";
+        else
+            textToToggle.text = "Do you want to enter the nursery?";
     }
 }
